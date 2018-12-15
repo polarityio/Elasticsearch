50054799cf8a054394e7540988762f0164f8aa97
Ed <ed@polarity.io>
2018-12-14 23:20:41 -0500

    Reformat code using prettier

--- config/config.js
+++ config/config.js
@@ -1,82 +1,80 @@
 module.exports = {
-    "name": "Elasticsearch",
-    "acronym":"ES",
-    "logging": { level: 'debug'},
-    "entityTypes": ['IPv4', 'IPv6'],
-    "styles": [
-        "./styles/es.less"
-    ],
-    "block": {
-        "component": {
-            "file": "./component/es.js"
-        },
-        "template": {
-            "file": "./template/es.hbs"
-        }
+  name: 'Elasticsearch',
+  acronym: 'ES',
+  logging: { level: 'debug' },
+  entityTypes: ['IPv4', 'IPv6'],
+  styles: ['./styles/es.less'],
+  block: {
+    component: {
+      file: './component/es.js'
     },
-    "options":[
-        {
-            "key"          : "host",
-            "name"         : "Elasticsearch Host",
-            "description"  : "URL for Elasticsearch instance",
-            "default"      : "",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "port",
-            "name"         : "Port for Elasticsearch rest service - default for Elasticsearch is 9200",
-            "description"  : "",
-            "default"      : "9200",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "username",
-            "name"         : "Username",
-            "description"  : "Elasticsearch Account Username",
-            "default"      : "",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "password",
-            "name"         : "Password",
-            "description"  : "Elasticsearch account password",
-            "default"      : "",
-            "type"         : "password",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "index",
-            "name"         : "Index for Elasticsearch",
-            "description"  : "Elasticsearch index you want searched for results",
-            "default"      : "",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "type",
-            "name"         : "Type",
-            "description"  : "Index type that you want searched",
-            "default"      : "",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "uiHostname",
-            "name"         : "UI Hostname",
-            "description"  : "UI Hostname of the Kibana instance you are running",
-            "default"      : "",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        }
-    ]
+    template: {
+      file: './template/es.hbs'
+    }
+  },
+  options: [
+    {
+      key: 'host',
+      name: 'Elasticsearch Host',
+      description: 'URL for Elasticsearch instance',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'port',
+      name: 'Port for Elasticsearch rest service - default for Elasticsearch is 9200',
+      description: '',
+      default: '9200',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'username',
+      name: 'Username',
+      description: 'Elasticsearch Account Username',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'password',
+      name: 'Password',
+      description: 'Elasticsearch account password',
+      default: '',
+      type: 'password',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'index',
+      name: 'Index for Elasticsearch',
+      description: 'Elasticsearch index you want searched for results',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'type',
+      name: 'Type',
+      description: 'Index type that you want searched',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'uiHostname',
+      name: 'UI Hostname',
+      description: 'UI Hostname of the Kibana instance you are running',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    }
+  ]
 };
--- integrations.js
+++ integrations.js
@@ -1,191 +1,213 @@
 'use strict';
 
-let request = require('request');
-let _ = require('lodash');
-let async = require('async');
+const request = require('request');
+const _ = require('lodash');
+const async = require('async');
 let log;
 
 function startup(logger) {
-    log = logger;
+  log = logger;
 }
 
-
 function doLookup(entities, options, cb) {
+  let lookupResults = [];
 
-
-    let lookupResults = [];
-
-    async.each(entities, function (entityObj, next) {
-        if (entityObj.isIPv4 || entityObj.isIPv6 && !entityObj.isPrivateIP) {
-            _lookupEntity(entityObj, options, function (err, result) {
-                if (err) {
-                    next(err);
-                } else {
-                    lookupResults.push(result); log.debug({result: result}, "Printing out the Results");
-                    next(null);
-                }
-            });
-        } else {
-            lookupResults.push({entity: entityObj, data: null}); //Cache the missed results
+  async.each(
+    entities,
+    function(entityObj, next) {
+      if (entityObj.isIPv4 || (entityObj.isIPv6 && !entityObj.isPrivateIP)) {
+        _lookupEntity(entityObj, options, function(err, result) {
+          if (err) {
+            next(err);
+          } else {
+            lookupResults.push(result);
+            log.debug({ result: result }, 'Printing out the Results');
             next(null);
-        }
-    }, function (err) {
-        cb(err, lookupResults);
-
-    });
+          }
+        });
+      } else {
+        lookupResults.push({ entity: entityObj, data: null }); //Cache the missed results
+        next(null);
+      }
+    },
+    function(err) {
+      cb(err, lookupResults);
+    }
+  );
 }
 
 function validateOptions(userOptions, cb) {
-    let errors = [];
-    if(typeof userOptions.host.value !== 'string' ||
-        (typeof userOptions.host.value === 'string' && userOptions.host.value.length === 0)){
-        errors.push({
-            key: 'host',
-            message: 'You must provide a valid Elastichsearch Hostname.'
-        })
-    }
+  let errors = [];
+  if (
+    typeof userOptions.host.value !== 'string' ||
+    (typeof userOptions.host.value === 'string' && userOptions.host.value.length === 0)
+  ) {
+    errors.push({
+      key: 'host',
+      message: 'You must provide a valid Elastichsearch Hostname.'
+    });
+  }
 
-    if(typeof userOptions.index.value !== 'string' ||
-        (typeof userOptions.index.value === 'string' && userOptions.index.value.length === 0)){
-        errors.push({
-            key: 'index',
-            message: 'You must provide the Index you want searched in Elasticsearch'
-        })
-    }
+  if (
+    typeof userOptions.index.value !== 'string' ||
+    (typeof userOptions.index.value === 'string' && userOptions.index.value.length === 0)
+  ) {
+    errors.push({
+      key: 'index',
+      message: 'You must provide the Index you want searched in Elasticsearch'
+    });
+  }
 
-    if(typeof userOptions.type.value !== 'string' ||
-        (typeof userOptions.type.value === 'string' && userOptions.type.value.length === 0)){
-        errors.push({
-            key: 'type',
-            message: 'You must provide the Index Type for your search'
-        })
-    }
+  if (
+    typeof userOptions.type.value !== 'string' ||
+    (typeof userOptions.type.value === 'string' && userOptions.type.value.length === 0)
+  ) {
+    errors.push({
+      key: 'type',
+      message: 'You must provide the Index Type for your search'
+    });
+  }
 
-    cb(null, errors);
+  cb(null, errors);
 }
 
-
-
 function _lookupEntity(entityObj, options, cb) {
-    let uri = options.host + ':' + options.port + '/' + options.index + '/' + options.type + '/_search?pretty';
+  let uri = options.host + ':' + options.port + '/' + options.index + '/' + options.type + '/_search?pretty';
 
-    var kibana = options.uiHostname;
+  var kibana = options.uiHostname;
 
-    let esData = {
-        "query": {
-            "term" : { "message" : entityObj.value }
-        }
+  let esData = {
+    query: {
+      term: { message: entityObj.value }
     }
+  };
 
+  log.debug('Printing out JSON payload %j', esData);
 
-    log.debug("Printing out JSON payload %j", esData);
+  request(
+    {
+      uri: uri,
+      method: 'GET',
+      json: esData
+    },
+    function(err, response, body) {
+      // check for an error
+      if (err) {
+        return;
+      }
 
-    request({
-        uri: uri,
-        method: 'GET',
-        json: esData
-    }, function (err, response, body) {
-        // check for an error
-        if (err) {
-            return;
-        }
+      log.debug('Checking body %j', body);
 
-        log.debug("Checking body %j", body);
+      if (response.statusCode === 404) {
+        cb(
+          _createJsonErrorPayload('Not Found', null, '404', '2A', 'Not Found', {
+            err: err
+          })
+        );
+        return;
+      }
 
-        if(response.statusCode === 404){
-            cb(_createJsonErrorPayload("Not Found", null, '404', '2A', 'Not Found', {
-                err: err
-            }));
-            return;
-        }
-
-        if(response.statusCode === 400){
-            cb(_createJsonErrorPayload("Invalid Search, please check search parameters", null, '400', '2A', 'Bad Request', {
-                err: err
-            }));
-            return;
-        }
-        if(response.statusCode === 409){
-            cb(_createJsonErrorPayload("There was a conflict with your search", null, '409', '2A', 'Conflict', {
-                err: err
-            }));
-            return;
-        }
-        if(response.statusCode === 503){
-            cb(_createJsonErrorPayload("Service is currently unavailable for search results", null, '503', '2A', '', {
-                err: err
-            }));
-            return;
-        }
-        if(response.statusCode === 500){
-            cb(_createJsonErrorPayload("Internal Server error, please check your instance", null, '500', '2A', 'Internal Server Error', {
-                err: err
-            }));
-            return;
-        }
-
-        if(response.body.hits.total === 0){
-            cb(null, {
-                entity: entityObj,
-                data: null
-            });
-        }
-
-
-        // The lookup results returned is an array of lookup objects with the following format
-        else {cb(null, {
-            // Required: This is the entity object passed into the integration doLookup method
-            entity: entityObj,
-            // Required: An object containing everything you want passed to the template
-            data: {
-                // Required: this is the string value that is displayed in the template
-                entity_name: entityObj.value,
-                // Required: These are the tags that are displayed in your template
-                summary: ["Number of Results:" + body.hits.total],
-                // Data that you want to pass back to the notification window details block
-                details: {
-                   hits: body.hits.total,
-                   url: kibana
-                }
+      if (response.statusCode === 400) {
+        cb(
+          _createJsonErrorPayload('Invalid Search, please check search parameters', null, '400', '2A', 'Bad Request', {
+            err: err
+          })
+        );
+        return;
+      }
+      if (response.statusCode === 409) {
+        cb(
+          _createJsonErrorPayload('There was a conflict with your search', null, '409', '2A', 'Conflict', {
+            err: err
+          })
+        );
+        return;
+      }
+      if (response.statusCode === 503) {
+        cb(
+          _createJsonErrorPayload('Service is currently unavailable for search results', null, '503', '2A', '', {
+            err: err
+          })
+        );
+        return;
+      }
+      if (response.statusCode === 500) {
+        cb(
+          _createJsonErrorPayload(
+            'Internal Server error, please check your instance',
+            null,
+            '500',
+            '2A',
+            'Internal Server Error',
+            {
+              err: err
             }
-        }); }
+          )
+        );
+        return;
+      }
 
-    });
+      if (response.body.hits.total === 0) {
+        cb(null, {
+          entity: entityObj,
+          data: null
+        });
+      }
+
+      // The lookup results returned is an array of lookup objects with the following format
+      else {
+        cb(null, {
+          // Required: This is the entity object passed into the integration doLookup method
+          entity: entityObj,
+          // Required: An object containing everything you want passed to the template
+          data: {
+            // Required: this is the string value that is displayed in the template
+            entity_name: entityObj.value,
+            // Required: These are the tags that are displayed in your template
+            summary: ['Number of Results:' + body.hits.total],
+            // Data that you want to pass back to the notification window details block
+            details: {
+              hits: body.hits.total,
+              url: kibana
+            }
+          }
+        });
+      }
+    }
+  );
 }
 
 // function that takes the ErrorObject and passes the error message to the notification window
-var _createJsonErrorPayload = function (msg, pointer, httpCode, code, title, meta) {
-    return {
-        errors: [
-            _createJsonErrorObject(msg, pointer, httpCode, code, title, meta)
-        ]
-    }
+var _createJsonErrorPayload = function(msg, pointer, httpCode, code, title, meta) {
+  return {
+    errors: [_createJsonErrorObject(msg, pointer, httpCode, code, title, meta)]
+  };
 };
 
 // function that creates the Json object to be passed to the payload
-var _createJsonErrorObject = function (msg, pointer, httpCode, code, title, meta) {
-    let error = {
-        detail: msg,
-        status: httpCode.toString(),
-        title: title,
-        code: 'Safe_' + code.toString()
+var _createJsonErrorObject = function(msg, pointer, httpCode, code, title, meta) {
+  let error = {
+    detail: msg,
+    status: httpCode.toString(),
+    title: title,
+    code: 'Safe_' + code.toString()
+  };
+
+  if (pointer) {
+    error.source = {
+      pointer: pointer
     };
+  }
 
-    if (pointer) {
-        error.source = {
-            pointer: pointer
-        };
-    }
+  if (meta) {
+    error.meta = meta;
+  }
 
-    if (meta) {
-        error.meta = meta;
-    }
-
-    return error;
+  return error;
 };
 
 module.exports = {
-    startup:startup,
-    doLookup: doLookup,
-    validateOptions: validateOptions
+  startup: startup,
+  doLookup: doLookup,
+  validateOptions: validateOptions
 };
