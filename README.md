529ea07c83ae22528ae696ebade140d18d961ec5
Ed <ed@polarity.io>
2019-01-11 10:39:09 -0500

    Finish 3.0

--- README.md
+++ README.md
@@ -1,46 +1,82 @@
 # Polarity Elasticsearch Integration
 
-Polarity's Elasticsearch integration allows a user to connect to an Elasticsearch instance. The integration returns the number of records for an IP that are present for a given entity. A user can also link out to their Kibana instance to view more information.
+Polarity's Elasticsearch integration can search your ES instance using a user provided search and return results via the Polarity Overlay Window or HUD.  Similar to Kibana, the integration will return highlighted document fields as well as the full `_source` field.  In addition, a table and JSON view are also provided. .
 
-![image](https://user-images.githubusercontent.com/22529325/28972966-bf8d9e4e-78ff-11e7-8788-6706f691113c.png)
+![53fb5dff-e139-4c47-a1d8-481f8b927df7](https://user-images.githubusercontent.com/306319/51043062-c28a6d00-158b-11e9-810d-4ae13c714841.GIF)
+
+> Note that by default the ES integration will receive ALL entity types.  It is important that you select the "Manage Integration Data" option and turn off entity types you do not want sent to your ES integration.
 
 ## Elasticsearch Integration Options
 
-### Hostname
+### Elasticsearch URL
 
-This setting is the hostname of your Elasicsearch instance. Please do not include the Scheme or Port if there is one. For example:
+
+URL for your Elasticsearch REST API including the schema and port if applicable (e.g., https://elastic.prod:9200)
 
 ```
-www.elasticsearchinstance.com
+https://elastic.prod:9200
 ```
-
-### Port
-
-The default for port for Elasticsearch is 9200. If you have changed your port when setting up your instance, please change the port here.
 
 ### Username
 
-Username set for an individual user or if you have a generic RestAPI user, you can set it here. It is only required if you have established credentials on your Elasticsearch instance
+Elasticsearch account username (Leave this blank if you are not using Basic Auth via Shield)
 
 ### Password
 
-Password set for the individual user or generic user. It is only required if you have established credentials on your Elasticsearch instance
+Elasticsearch account password (Leave this blank if you are not using Basic Auth via Shield)
 
-### Index
+### Index for Elasticsearch
 
-Provide the Index with Elasticsearch that you want to search against.
-
-### Type
-
-Provide the type of index that you are performing your searches against.
-
-
-### UI Hostname
-
-This the exact hostname that you go to in order to access the Splunk User-Interface. If there is a port or a protocal used, please ensure they are included.  For example:
+Comma delimited list of Elasticsearch indexes you want searched for results (no spaces between commas)
 
 ```
-https://www.mykibana.com
+kibana_sample_data_logs,kibana_sample_data_flights
+```
+
+### Search Query
+
+The search query to execute as JSON. The top level property should be a `query` object and must be a valid JSON search request when sent to the ES `_search` REST endpoint.  The search query can make use of the templated variable `{{entity}}` which will be replaced by the entity recognized on the user's screen.
+
+As an example, with the search query is defined as:
+
+```
+{"query": { "simple_query_string": { "query": "\"{{entity}}\"" } }, "from": 0, "size": 10, "sort": [ {"timestamp": "desc" } ] } }
+```
+
+If the user has the IP 8.8.8.8 on their screen the integration will execute the following query:
+
+```
+{"query": { "simple_query_string": { "query": "\"8.8.8.8\"" } }, "from": 0, "size": 10, "sort": [ {"timestamp": "desc" } ] } }
+```
+
+### Enable Highlighting
+
+If checked, the integration will display highlighted search terms via the Elasticsearch Highlighter.  For more information on the Elasticsearch Highlighter please see the following documentation: https://www.elastic.co/guide/en/elasticsearch/reference/current/search-request-highlighting.html
+
+### Highlight Query
+
+The highlighter query to execute when a user clicks to view additional details. The top level property should be a `query` object. This query should typically match the query portion of your `Search Query`. Highlighting will attempt to highlight against all fields and will return the first 10 results. Only runs if the `Enable Highlighting` option is checked
+
+```
+{"query": { "simple_query_string": { "query": "\"{{entity}}\"" } } }
+```
+
+### Summary Fields
+
+Comma delimited list of "_source" fields to include as part of the summary (no spaces between commas). These fields must be returned by your search query.  Defaults to `index`.
+
+### Document Title Field
+
+"_source" field to use as the title for each returned document in the details template. This field must be returned by your search query.  Defaults to `timestamp`.
+
+### Kibana URL
+
+URL for your Elasticsearch Kibana interface including the schema and port if applicable (e.g., https://elastic.prod:9243/app/kibana).  If left blank no link to Kibana will be provided.
+
+> Note that this link is not yet able to link directly to a returned document id but only takes you to the Kibana web interface.
+
+```
+https://elastic.prod:9243/app/kibana
 ```
 
 ## Installation Instructions
---
+++ integration.js
@@ -0,0 +1,470 @@
+'use strict';
+
+const request = require('request');
+const _ = require('lodash');
+const async = require('async');
+const fs = require('fs');
+const config = require('./config/config');
+
+const entityTemplateReplacementRegex = /{{entity}}/g;
+const MAX_ENTITIES_PER_LOOKUP = 10;
+const MAX_PARALLEL_LOOKUPS = 5;
+
+let log;
+let requestWithDefaults;
+
+function startup(logger) {
+  log = logger;
+
+  const defaults = {};
+
+  if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
+    defaults.cert = fs.readFileSync(config.request.cert);
+  }
+
+  if (typeof config.request.key === 'string' && config.request.key.length > 0) {
+    defaults.key = fs.readFileSync(config.request.key);
+  }
+
+  if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
+    defaults.passphrase = config.request.passphrase;
+  }
+
+  if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
+    defaults.ca = fs.readFileSync(config.request.ca);
+  }
+
+  if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
+    defaults.proxy = config.request.proxy;
+  }
+
+  if (typeof config.request.rejectUnauthorized === 'boolean') {
+    defaults.rejectUnauthorized = config.request.rejectUnauthorized;
+  }
+
+  requestWithDefaults = request.defaults(defaults);
+}
+
+function doLookup(entities, options, cb) {
+  let self = this;
+  let lookupResults = [];
+  const filteredEntities = [];
+  const lookupTasks = [];
+
+  entities.forEach((entityObj) => {
+    if (entityObj.isIP) {
+      if (!entityObj.isPrivateIP) {
+        filteredEntities.push(entityObj);
+      }
+    } else {
+      filteredEntities.push(entityObj);
+    }
+  });
+
+  if (filteredEntities.length === 0) {
+    return cb(null, []);
+  }
+
+  const summaryFields = options.summaryFields.split(',');
+  log.trace({ options }, 'options');
+  const entityGroups = _.chunk(filteredEntities, MAX_ENTITIES_PER_LOOKUP);
+  entityGroups.forEach((entityGroup) => {
+    lookupTasks.push(_lookupEntityGroup.bind(self, entityGroup, summaryFields, options));
+  });
+
+  async.parallelLimit(lookupTasks, MAX_PARALLEL_LOOKUPS, (err, results) => {
+    if (err) {
+      log.error(err, 'Error running lookup');
+      return cb(err);
+    }
+
+    results.forEach((result) => {
+      lookupResults = lookupResults.concat(result);
+    });
+
+    cb(null, lookupResults);
+  });
+}
+
+function onDetails(lookupObject, options, cb) {
+  if (options.highlightEnabled === false) {
+    return cb(null, lookupObject.data);
+  }
+
+  const documentIds = lookupObject.data.details.results.map((item) => {
+    return item.hit._id;
+  });
+
+  const requestOptions = {
+    uri: `${options.url}/${options.index}/_search`,
+    method: 'GET',
+    body: _buildOnDetailsQuery(lookupObject.entity, documentIds, options),
+    json: true
+  };
+
+  if (
+    typeof options.username === 'string' &&
+    options.username.length > 0 &&
+    typeof options.password === 'string' &&
+    options.password.length > 0
+  ) {
+    requestOptions.auth = {
+      user: options.username,
+      pass: options.password
+    };
+    // requestOptions.headers.Authorization =
+    //   'Basic ' + Buffer.from(`${options.username}:${options.password}`).toString('base64');
+  }
+
+  log.debug({ onDetailsQuery: requestOptions }, 'onDetails Request Payload');
+  lookupObject.data.details.highlights = {};
+  requestWithDefaults(requestOptions, function(httpErr, response, body) {
+    if (httpErr) {
+      return cb(httpErr);
+    }
+
+    body.hits.hits.forEach((hit) => {
+      const resultHighlights = [];
+      for (const [fieldName, fieldValues] of Object.entries(hit.highlight)) {
+        if (!fieldName.endsWith('.keyword')) {
+          resultHighlights.push({
+            fieldName,
+            fieldValues
+          });
+        }
+      }
+      lookupObject.data.details.highlights[hit._id] = resultHighlights;
+    });
+
+    log.debug({ onDetails: lookupObject.data }, 'onDetails data result');
+    cb(null, lookupObject.data);
+  });
+}
+
+function _buildOnDetailsQuery(entityObj, documentIds, options) {
+  const highlightQuery = options.highlightQuery.replace(entityTemplateReplacementRegex, entityObj.value);
+  return {
+    _source: false,
+    query: {
+      ids: {
+        type: '_doc',
+        values: documentIds
+      }
+    },
+    highlight: {
+      fields: {
+        '*': {}
+      },
+      highlight_query: JSON.parse(highlightQuery).query,
+      pre_tags: ['<span class="highlight">'],
+      post_tags: ['</span>'],
+      encoder: 'html',
+      fragment_size: 200
+    },
+    from: 0,
+    size: 10
+  };
+}
+
+/**
+ * Returns an elasticsearch query that uses the multi-search format:
+ *
+ * https://www.elastic.co/guide/en/elasticsearch/reference/current/search-multi-search.html
+ *
+ * @param entities
+ * @param options
+ * @returns {Object}
+ * @private
+ */
+function _buildDoLookupQuery(entities, options) {
+  let multiSearchString = '';
+  const multiSearchQueries = [];
+
+  entities.forEach((entityObj) => {
+    const query = options.query.replace(entityTemplateReplacementRegex, entityObj.value);
+    multiSearchString += `{}\n${query}\n`;
+    multiSearchQueries.push(query);
+  });
+  return { multiSearchString, multiSearchQueries };
+}
+
+function _getSummaryTags(searchItemResult, summaryFields) {
+  const tags = new Set();
+
+  searchItemResult.hits.hits.forEach((hit) => {
+    if (!hit._source) {
+      tags.add('Missing _source field');
+    } else {
+      summaryFields.forEach((field) => {
+        const summaryField = _.get(hit._source, field);
+        if (summaryField) {
+          tags.add(summaryField);
+        }
+      });
+    }
+  });
+
+  return [...tags];
+}
+
+function _lookupEntityGroup(entityGroup, summaryFields, options, cb) {
+  const queryObject = _buildDoLookupQuery(entityGroup, options);
+  const requestOptions = {
+    uri: `${options.url}/${options.index}/_msearch`,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-ndjson'
+    },
+    body: queryObject.multiSearchString
+  };
+
+  if (options.username && options.password) {
+    requestOptions.headers.Authorization =
+      'Basic ' + Buffer.from(`${options.username}:${options.password}`).toString('base64');
+  }
+
+  log.debug({ requestOptions: requestOptions }, 'lookupEntityGroup Request Payload');
+
+  requestWithDefaults(requestOptions, function(httpErr, response, body) {
+    if (httpErr) {
+      return cb({
+        err: httpErr,
+        detail: 'Error making HTTP request'
+      });
+    }
+
+    const jsonBody = _parseBody(body);
+    if (jsonBody === null) {
+      return cb(
+        _createJsonErrorPayload('JSON Parse Error of HTTP Response', null, '404', '1', 'JSON Parse Error', {
+          body: body
+        })
+      );
+    }
+
+    const restError = _handleRestErrors(response, jsonBody);
+    if (restError) {
+      return cb(restError);
+    }
+
+    const entityGroupResults = [];
+
+    jsonBody.responses.forEach((searchItemResult, index) => {
+      if (_isMiss(searchItemResult)) {
+        entityGroupResults.push({
+          entity: entityGroup[index],
+          data: null
+        });
+      } else {
+        // wrap the hit in another object so that the font-end integration component can inject properties to track
+        // various states without mutating the raw hit result returned from ES.  The raw hit result is stored in `hit`
+        const hits = searchItemResult.hits.hits.map((hit) => {
+          return {
+            hit: hit
+          };
+        });
+        entityGroupResults.push({
+          entity: entityGroup[index],
+          data: {
+            summary: _getSummaryTags(searchItemResult, summaryFields),
+            details: {
+              results: hits,
+              queries: queryObject.multiSearchQueries
+            }
+          }
+        });
+      }
+    });
+
+    cb(null, entityGroupResults);
+  });
+}
+
+/**
+ * Body is not parsed into JSON for us because the request we make is not JSON.  As a result,
+ * we have to parse body ourselves.
+ * @private
+ */
+function _parseBody(body) {
+  if (body) {
+    try {
+      return JSON.parse(body);
+    } catch (e) {
+      return null;
+    }
+  }
+  return null;
+}
+
+function _isMiss(responseObject) {
+  if (
+    responseObject &&
+    responseObject.hits &&
+    Array.isArray(responseObject.hits.hits) &&
+    responseObject.hits.hits.length > 0
+  ) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * HTTP Error Codes taken from:
+ * https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/errors.html
+ * @param response
+ * @param body
+ * @returns {*}
+ * @private
+ */
+function _handleRestErrors(response, body) {
+  switch (response.statusCode) {
+    case 403:
+      return _createJsonErrorPayload('Access to the resource is forbidden', null, '404', '1', 'Forbidden', {
+        body: body
+      });
+      break;
+    case 404:
+      return _createJsonErrorPayload('Not Found', null, '404', '1', 'Not Found', {
+        body: body
+      });
+      break;
+    case 400:
+      return _createJsonErrorPayload(
+        'Invalid Search, please check search parameters',
+        null,
+        '400',
+        '2',
+        'Bad Request',
+        {
+          body: body
+        }
+      );
+      break;
+    case 409:
+      return _createJsonErrorPayload('There was a conflict with your search', null, '409', '3', 'Conflict', {
+        body: body
+      });
+      break;
+    case 503:
+      return _createJsonErrorPayload(
+        'Service is currently unavailable for search results',
+        null,
+        '503',
+        '4',
+        'Service Unavailable',
+        {
+          body: body
+        }
+      );
+    case 500:
+      return _createJsonErrorPayload(
+        'Internal Server error, please check your instance',
+        null,
+        '500',
+        '5',
+        'Internal Server Error',
+        {
+          body: body
+        }
+      );
+      break;
+    case 200:
+      if (!Array.isArray(body.responses)) {
+        return _createJsonErrorPayload(
+          'Unexpected Response Payload Format.  "body.responses" should be an array',
+          null,
+          response.statusCode,
+          '6',
+          'Unexpected HTTP Error',
+          {
+            body: body
+          }
+        );
+      } else {
+        return null;
+      }
+      break;
+  }
+
+  return _createJsonErrorPayload(
+    'Unexpected HTTP Response Status Code',
+    null,
+    response.statusCode,
+    '7',
+    'Unexpected HTTP Error',
+    {
+      body: body
+    }
+  );
+}
+
+function validateOptions(userOptions, cb) {
+  let errors = [];
+  if (
+    typeof userOptions.url.value !== 'string' ||
+    (typeof userOptions.url.value === 'string' && userOptions.url.value.length === 0)
+  ) {
+    errors.push({
+      key: 'host',
+      message: 'You must provide a valid Elasticsearch URL.'
+    });
+  }
+
+  if (
+    typeof userOptions.index.value !== 'string' ||
+    (typeof userOptions.index.value === 'string' && userOptions.index.value.length === 0)
+  ) {
+    errors.push({
+      key: 'index',
+      message: 'You must provide the Index you want searched in Elasticsearch'
+    });
+  }
+
+  if (
+    typeof userOptions.query.value !== 'string' ||
+    (typeof userOptions.query.value === 'string' && userOptions.query.value.length === 0)
+  ) {
+    errors.push({
+      key: 'type',
+      message: 'You must provide a valid Search Query'
+    });
+  }
+
+  cb(null, errors);
+}
+
+// function that takes the ErrorObject and passes the error message to the notification window
+var _createJsonErrorPayload = function(msg, pointer, httpCode, code, title, meta) {
+  return {
+    errors: [_createJsonErrorObject(msg, pointer, httpCode, code, title, meta)]
+  };
+};
+
+// function that creates the Json object to be passed to the payload
+var _createJsonErrorObject = function(msg, pointer, httpCode, code, title, meta) {
+  let error = {
+    detail: msg,
+    status: httpCode.toString(),
+    title: title,
+    code: 'ES_' + code.toString()
+  };
+
+  if (pointer) {
+    error.source = {
+      pointer: pointer
+    };
+  }
+
+  if (meta) {
+    error.meta = meta;
+  }
+
+  return error;
+};
+
+module.exports = {
+  startup: startup,
+  doLookup: doLookup,
+  validateOptions: validateOptions,
+  onDetails: onDetails
+};
